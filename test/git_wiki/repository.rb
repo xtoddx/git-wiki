46d0cfa04c973d23e4dd3da2b4a7fa3e245dd9fd
Todd Willey <todd@lapex.(none)>
2009-03-22 14:34:59 -0400

    Populate the test_data branch with some, well, test data.

--- test/git_wiki/repository.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.dirname(__FILE__), 'helper')
-
-context 'A Repository' do
-  it 'should provide an instance initialized to GitWiki.repository_url' do
-    GitWiki.repository_url = 'test/test_wiki'
-    assert GitWiki::Repository.instance
-  end
-end
